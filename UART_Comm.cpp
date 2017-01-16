f283fc8274269c23b96207a5bf42155f821841b0
Qinru <qinruli2@illinois.edu>
2017-01-15 18:47:17 -0600

    baud rate 1M complete

--- UART_Comm.cpp
+++ UART_Comm.cpp
@@ -7,20 +7,21 @@
 #include <fcntl.h>
 #include <termios.h> 
 
-#define switch 1
+#define SWITCH 1
 #define SWITCH_ON 1	 // SWITCH_ON = RX enable, RPi in receiving mode. 
 #define SWITCH_OFF 0   // SWITCH_OFF = TX enable, RPi in transmitting mode. 
 									   
 using namespace std;
 
+// This function will perform a data writting/transmitting 
 int writeData(int fd, char *data, int dataLen){
-	digitalWrite(switch, SWITCH_OFF);
+	digitalWrite(SWITCH, SWITCH_OFF);
 	delayMicroseconds(100);				//Need delay some time before the GPIO switch is truely turned off. 
 										//I guess 20us is the limited time (reading from oscilloscope)
 	int result = write(fd, data, dataLen);
 	delayMicroseconds(80*dataLen);		// After sending the message, need delay some time before the GPIO switch is truely turned on.
 										//I guess 400us is the limited time for transimitting 5 bytes (by tried and error)
-	digitalWrite(switch, SWITCH_ON);
+	digitalWrite(SWITCH, SWITCH_ON);
 	return result;
 }
 
@@ -28,26 +29,93 @@ int writeData(int fd, char *data, int dataLen){
 	
 //}
 
+// This function will initialize the uart serial port and return this corresponding filestream. 
+int uartSerialSetup(char* device, int baud){
+	//Following code are all copied based on http://www.raspberry-projects.com/pi/programming-in-c/uart-serial-port/using-the-uart
+	int uart0_filestream = -1;
+	//OPEN THE UART
+	//The flags (defined in fcntl.h):
+	//	Access modes (use 1 of these):
+	//		O_RDONLY - Open for reading only.
+	//		O_RDWR - Open for reading and writing.
+	//		O_WRONLY - Open for writing only.
+	//
+	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
+	//											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
+	//											immediately with a failure status if the output can't be written immediately.
+	//
+	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
+	uart0_filestream = open(device, O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
+	if (uart0_filestream == -1)
+	{
+		//ERROR - CAN'T OPEN SERIAL PORT
+		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+	}
+	
+	//CONFIGURE THE UART
+	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
+	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
+	//	CSIZE:- CS5, CS6, CS7, CS8
+	//	CLOCAL - Ignore modem status lines
+	//	CREAD - Enable receiver
+	//	IGNPAR = Ignore characters with parity errors
+	//	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
+	//	PARENB - Parity enable
+	//	PARODD - Odd parity (else even)
+	struct termios options;
+	tcgetattr(uart0_filestream, &options);
+	// Set baud rate
+	switch(baud){
+		case 9600:
+			options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		
+			break;
+		case 115200:
+			options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;		
+			break;
+		case 1000000:
+			options.c_cflag = B1000000 | CS8 | CLOCAL | CREAD | ~PARENB;		
+			break;
+		default:
+			options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;		
+			break;
+	}
+	
+	// Set other stuffs
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+	tcflush(uart0_filestream, TCIFLUSH);
+	tcsetattr(uart0_filestream, TCSANOW, &options);
+	
+	return uart0_filestream;
+}
+
 //To compile: g++ UART_Comm.cpp -Wall -o uart -lwiringPi
 int main(){
 	// UART Setup
-	int baud = 115200;	//baud rate
+	// Note: wiringPi does not support 1M Baud Rate, so I have to re-write a serialSetup function by myself.
+	//		 Following the code in http://www.raspberry-projects.com/pi/programming-in-c/uart-serial-port/using-the-uart
+	// 		 Now this "uartSerialSetup" can support 1M Baud Rate, it is also capable of setting up more custormized specification such as parity. 
+	int baud = 1000000;	//baud rate
+	int fd = uartSerialSetup("/dev/ttyS0",baud);
+	/*
 	int fd = serialOpen("/dev/ttyS0",baud);
 	if(( fd < 0 )){
 		std::cout << "Open file failed" << std::endl;
 		return 0;
 	}
+	*/
 	
 	//Initialize GPIO switch
 	wiringPiSetup();
-	pinMode(switch, OUTPUT);
-	digitalWrite(switch, SWITCH_ON);
+	pinMode(SWITCH, OUTPUT);
+	digitalWrite(SWITCH, SWITCH_ON);
 	
 	// UART test
 	while(1){
 		
-		char data_2config[5] = {0xFA, 0xFF, 0x30, 0x00, 0xD1};
-		writeData(fd, data_2config, 5);
-		delay(5);
+		char data_2config[6] = {0xFF, 0xFF, 0x01, 0x02, 0x01, 0xFB};
+		writeData(fd, data_2config, 6);
+		delay(1);
 	}
 }
